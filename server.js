9e3670eba582669f1db3b6c72112890ab4da1e11
sruthimildred <chatwithsruthimildred@gmail.com>
2022-02-10 18:08:49 +0530

    new changes

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { MongoClient } from 'mongodb';
-// import cors from "cors"
+const cors = require("cors");
+require("dotenv").config();
+
 
 const app = express();
+
+
+
 
 const PORT = process.env.PORT || 8000
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cors());
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // const MONGODB_URI="mongodb://localhost:27017"
 const withDB = async (operations, res) => {
